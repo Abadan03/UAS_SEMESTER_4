b9ed7541eb2d0502b402816e5f2f1e78bc310a29
aisyahnabila <aisyahnabila2.an@gmail.com>
2023-07-17 20:43:25 +0700

    tambah view homepage dengan CSS dan JS

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $pageTitle = "Home Page";
-
-    return view('index', compact('pageTitle'));
+    return view('home');
 });
