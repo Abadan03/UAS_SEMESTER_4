4808fc5ea44691922fdd7ad55eabb5b318ebfbda
Dinar Abadan <dinarabadan03@gmail.com>
2023-07-21 00:06:20 +0700

    menambahkan fitur login dan membetulkan section design

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home_auth');
+    // Route::get('/home', function () {
+        //     return view('home');
+        // });
+
+
+    });
+Route::get('/home', [IndexController::class, 'index'])->name('home');
+
+
