60256c23d203e3d3cf1bad5db11a782718cd93b2
aisyahnabila <aisyahnabila2.an@gmail.com>
2023-07-20 21:56:26 +0700

    error vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $pageTitle }}</title>
+    {{-- <title>{{ $pageTitle }}</title> --}}
     @vite('resources/sass/app.scss')
 
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 </head>
+
 <body>
     @include('layouts.nav')
     <div class="min-vw-100">
@@ -17,4 +19,5 @@
     </div>
     @vite('resources/js/app.js')
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+
 });
